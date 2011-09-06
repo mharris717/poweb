6e5dfc009b2e9eb3d7b4270df351db369bd99c5b
Mike Harris <mharris@indians.com>
2011-09-06 18:12:34 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoWeb::Application.config.secret_token = '569270c11317b0801d338afed6ddbe4ff4fc012b63e606b9efeeec1159b1e38058548bbc8944ead1ccaf05e6ac080931f2a92b0fc949b5e60222309e31b370bb'
